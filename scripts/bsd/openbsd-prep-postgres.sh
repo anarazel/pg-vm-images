6253d543864bc81f847ef6640e5d0ff4c07dbeae
Nazir Bilal Yavuz <byavuz81@gmail.com>
2022-11-23 14:17:17 +0300

    openbsd: Add noatime and softdep to the mount points

--- scripts/bsd/openbsd-prep-postgres.sh
+++ scripts/bsd/openbsd-prep-postgres.sh
@@ -28,6 +28,18 @@ pkg_add -I \
     openldap-client--gssapi \
     openldap-server--gssapi
 
+#####
+# Add 'noatime' and 'softdep' to the mount points
+# https://man.openbsd.org/mount.8
+FSTAB_FILE=/etc/fstab
+OPTIONS='noatime,softdep'
+
+cat ${FSTAB_FILE}
+echo "Enabling mount option(s) \"${OPTIONS}\""
+sed -i -e "/ffs/  s/rw/rw,${OPTIONS}/" ${FSTAB_FILE}
+cat ${FSTAB_FILE}
+#####
+
 # Set kernel parameters for running postgres tests
 echo "/sbin/sysctl kern.seminfo.semmni=2048" >> /etc/rc.local
 echo "/sbin/sysctl kern.seminfo.semmns=32768" >> /etc/rc.local
