eddc8c04fd4e135b21ca8e43c45fe1ebe515ac88
Nazir Bilal Yavuz <byavuz81@gmail.com>
2023-02-07 22:30:01 +0300

    mingw: Fix IPC:Run installation on MinGW

--- scripts/windows_install_mingw64.ps1
+++ scripts/windows_install_mingw64.ps1
@@ -19,9 +19,12 @@ msys 'pacman --noconfirm -Scc' ;
 
 echo 'installing packages' ;
 msys 'pacman -S --needed --noconfirm git bison flex make diffutils \
-    ucrt64/mingw-w64-ucrt-x86_64-{ccache,gcc,icu,libxml2,libxslt,lz4,meson,perl,pkg-config,zlib}' ;
+    ucrt64/mingw-w64-ucrt-x86_64-{ccache,gcc,icu,libxml2,libxslt,lz4,make,meson,perl,pkg-config,zlib}' ;
 msys 'pacman -Scc --noconfirm'
 
 # Install perl modules to enable tap tests
 msys 'where perl'
 msys '(echo; echo o conf recommends_policy 0; echo notest install IPC::Run) | cpan'
+
+# Check if IPC::Run is installed correctly
+msys 'perl -mIPC::Run -e 1'
