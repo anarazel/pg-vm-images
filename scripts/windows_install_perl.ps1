cbd5d46f2fb7b28efb126ddac64d12711247dfa8
Nazir Bilal Yavuz <byavuz81@gmail.com>
2024-10-08 10:14:12 +0300

    Bump Windows Perl version to 5.40.0.1
    Fixes the problem at
    https://www.postgresql.org/message-id/87ttdq9vl5.fsf%40wibble.ilmari.org.

--- scripts/windows_install_perl.ps1
+++ scripts/windows_install_perl.ps1
@@ -1,11 +1,12 @@
 $ErrorActionPreference = "Stop"
 
 $perl_version = $Env:TEMP_PERL_VERSION
+$perl_version_wo_dots = $perl_version.Replace(".", "")
 $filepath = "$Env:TEMP/perl.zip"
 
 echo "downloading perl $perl_vesion"
 curl.exe -fsSL -o "$filepath" `
-    https://strawberryperl.com/download/$perl_version/strawberry-perl-$perl_version-64bit-portable.zip
+    https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_${perl_version_wo_dots}_64bit_UCRT/strawberry-perl-${perl_version}-64bit-portable.zip
 if (!$?) { throw 'cmdfail' }
 
 echo "installing perl $perl_vesion"
