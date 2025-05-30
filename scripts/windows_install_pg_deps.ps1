404844552e4d24a7a417ceabf6370c2766eb7024
Nazir Bilal Yavuz <byavuz81@gmail.com>
2025-05-30 15:28:05 +0300

    windows: Upgrade VS 2019 to VS 2022
    Per @macdice's request. New APIs are available in the VS 2022.

--- scripts/windows_install_pg_deps.ps1
+++ scripts/windows_install_pg_deps.ps1
@@ -12,7 +12,7 @@ function DownloadAndInstallDependency($DependencyName, $SourceUri)
     Remove-Item "c:\$($DependencyName).zip" -Force
 }
 
-DownloadAndInstallDependency "icu" "https://github.com/unicode-org/icu/releases/download/release-71-1/icu4c-71_1-Win64-MSVC2019.zip";
+DownloadAndInstallDependency "icu" "https://github.com/unicode-org/icu/releases/download/release-77-1/icu4c-77_1-Win64-MSVC2022.zip";
 DownloadAndInstallDependency "lz4" "https://github.com/lz4/lz4/releases/download/v1.9.3/lz4_win64_v1_9_3.zip";
 DownloadAndInstallDependency "zlib" "http://gnuwin32.sourceforge.net/downlinks/zlib-lib-zip.php";
 DownloadAndInstallDependency "zstd" "https://github.com/facebook/zstd/releases/download/v1.5.2/zstd-v1.5.2-win64.zip";
