220223a9a59fe7cb712dad7719ff4af71b2e1635
Nazir Bilal Yavuz <byavuz81@gmail.com>
2025-11-03 14:08:13 +0300

    Bump Windows Perl version to 5.42.0.1

--- scripts/windows_install_perl.ps1
+++ scripts/windows_install_perl.ps1
@@ -6,7 +6,7 @@ $filepath = "$Env:TEMP/perl.zip"
 
 echo "downloading perl $perl_vesion"
 curl.exe -fsSL -o "$filepath" `
-    https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_${perl_version_wo_dots}_64bit_UCRT/strawberry-perl-${perl_version}-64bit-portable.zip
+    https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_${perl_version_wo_dots}_64bit/strawberry-perl-${perl_version}-64bit-portable.zip
 if (!$?) { throw 'cmdfail' }
 
 echo "installing perl $perl_vesion"
