79eb3417c59b05b8accad9d188da12c9d77c4a90
Andres Freund <andres@anarazel.de>
2026-06-12 14:16:19 -0400

    linux: Move list of to be installed packages into an array
    This will make it a bit easier in a subsequent commit to make the set of
    packages getting installed determined by an argument.

--- scripts/linux_debian_install_deps.sh
+++ scripts/linux_debian_install_deps.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+# -*-indent-tabs-mode: nil; -*-
 
 set -e
 
@@ -22,85 +23,115 @@ if [ "$MAJOR_DEBIAN_VERSION" -lt 12 ]; then
   exit 1
 fi
 
-apt-get -y install --no-install-recommends \
-  procps \
-  time \
-  \
-  build-essential \
-  gdb \
-  git \
-  make \
-  meson \
-  perl \
-  pkg-config \
-  \
-  bison \
-  ccache \
-  clang \
-  '?exact-name(clang-16)' \
-  flex \
-  g++ \
-  gcc \
-  gettext \
-  python3-pip \
-  \
-  gnupg \
-  libio-pty-perl \
-  libipc-run-perl \
-  libmodule-signature-perl \
-  python3-cryptography \
-  python3-packaging \
-  python3-pytest \
-  python3-setuptools \
-  \
-  libcurl4-openssl-dev \
-  libicu-dev \
-  libkrb5-*-heimdal \
-  libkrb5-dev \
-  libldap2-dev \
-  liblz4-dev \
-  libnuma-dev \
-  libossp-uuid-dev \
-  libpam-dev \
-  libperl-dev \
-  libpython3-dev \
-  libreadline-dev \
-  libselinux-dev \
-  libssl-dev \
-  libsystemd-dev \
-  liburing-dev \
-  libxml2-dev \
-  libxslt1-dev \
-  libzstd-dev \
-  '?name(llvm-16-dev)' \
-  llvm-dev \
-  systemtap-sdt-dev \
-  tcl-dev \
-  uuid-dev \
-  \
-  krb5-admin-server \
-  krb5-kdc \
-  krb5-user \
-  ldap-utils \
-  locales-all \
-  lz4 \
-  slapd \
-  zstd \
-  \
-  libz-mingw-w64-dev \
+# Collect list of packages in an array and then install later, makes it easier
+# to add comments and conditionallity
+packages=()
+
+# Generic sysadmin stuff that's useful for timing, debugging etc
+packages+=(
+    gdb
+    procps
+    time
+)
+
+# Basic build tools
+packages+=(
+    build-essential
+    git
+    make
+    meson
+    perl
+    pkg-config
+)
+
+# Compilers, build-time tooling
+packages+=(
+    bison
+    ccache
+    flex
+    g++
+    gcc
+    gettext
+    python3-pip
+)
+
+# Compilers for building with LLVM support
+packages+=(
+    clang
+    '?exact-name(clang-16)'
+)
+
+# Basic test dependencies
+packages+=(
+    gnupg
+    libio-pty-perl
+    libipc-run-perl
+    libmodule-signature-perl
+    python3-cryptography
+    python3-packaging
+    python3-pytest
+    python3-setuptools
+)
+
+# Compile time library dependencies
+packages+=(
+    libcurl4-openssl-dev
+    libicu-dev
+    libkrb5-*-heimdal
+    libkrb5-dev
+    libldap2-dev
+    liblz4-dev
+    libnuma-dev
+    libossp-uuid-dev
+    libpam-dev
+    libperl-dev
+    libpython3-dev
+    libreadline-dev
+    libselinux-dev
+    libssl-dev
+    libsystemd-dev
+    liburing-dev
+    libxml2-dev
+    libxslt1-dev
+    libzstd-dev
+    '?name(llvm-16-dev)'
+    llvm-dev
+    systemtap-sdt-dev
+    tcl-dev
+    uuid-dev
+)
+
+# Full test dependencies
+packages+=(
+  krb5-admin-server
+  krb5-kdc
+  krb5-user
+  ldap-utils
+  locales-all
+  lz4
+  slapd
+  zstd
+)
+
+# Cross building windows
+packages+=(
+  libz-mingw-w64-dev
   mingw-w64-tools
+)
 
 # g++-mingw-w64-x86-64-win32 and gcc-mingw-w64-x86-64-win32 packages have
 # missing some functions in the headers starting from trixie, install ucrt64
 # versions on these releases.
 if [ "$MAJOR_DEBIAN_VERSION" -lt "13" ] ; then
-  apt-get -y install --no-install-recommends \
-    g++-mingw-w64-x86-64-win32 \
-    gcc-mingw-w64-x86-64-win32
+    packages+=(
+        g++-mingw-w64-x86-64-win32
+        gcc-mingw-w64-x86-64-win32
+    )
 else
-  apt-get -y install --no-install-recommends \
-    g++-mingw-w64-ucrt64 \
-    gcc-mingw-w64-ucrt64
+    packages+=(
+	g++-mingw-w64-ucrt64
+        gcc-mingw-w64-ucrt64
+    )
 fi
 
 if [ $(dpkg --print-architecture) = "amd64" ] ; then
@@ -112,29 +143,34 @@ if [ $(dpkg --print-architecture) = "amd64" ] ; then
   # they conflict with the amd64 variants
   dpkg --add-architecture i386
   apt-get update
-  apt-get -y install --no-install-recommends --no-remove \
-    gcc-multilib \
-    \
-    libcurl4-openssl-dev:i386 \
-    libicu-dev:i386 \
-    libkrb5-*-heimdal:i386 \
-    libkrb5-dev:i386 \
-    libldap2-dev:i386 \
-    liblz4-dev:i386 \
-    libpam-dev:i386 \
-    libperl-dev:i386 \
-    libpython3-dev:i386 \
-    libreadline-dev:i386 \
-    libselinux-dev:i386 \
-    libssl-dev:i386 \
-    libsystemd-dev:i386 \
-    liburing-dev:i386 \
-    libxml2-dev:i386 \
-    libxslt1-dev:i386 \
-    libzstd-dev:i386 \
-    tcl-dev:i386 \
-    uuid-dev:i386
+
+  packages+=(
+      gcc-multilib
+
+      libcurl4-openssl-dev:i386
+      libicu-dev:i386
+      libkrb5-*-heimdal:i386
+      libkrb5-dev:i386
+      libldap2-dev:i386
+      liblz4-dev:i386
+      libpam-dev:i386
+      libperl-dev:i386
+      libpython3-dev:i386
+      libreadline-dev:i386
+      libselinux-dev:i386
+      libssl-dev:i386
+      libsystemd-dev:i386
+      liburing-dev:i386
+      libxml2-dev:i386
+      libxslt1-dev:i386
+      libzstd-dev:i386
+      tcl-dev:i386
+      uuid-dev:i386
+  )
 fi
+
+apt-get -y install --no-install-recommends "${packages[@]}"
+
 
 echo "packages installed, performing some rude cleanup"
 
